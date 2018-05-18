c0421f5ba0664e63998804ece02608e83d32b64a
asakusuma <asa.kusuma@gmail.com>
2018-05-18 15:19:44 -0700

    Fix standalone test server script

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,9 @@ const serverApi = new ExpressTestServer();
 
 const EXPRESS_PORT = 3000;
 
-boot(serverApi).listen(EXPRESS_PORT, () => {
+const server = boot(serverApi);
+
+const handle = server.listen(EXPRESS_PORT, () => {
+  serverApi.attach(handle, EXPRESS_PORT);
   console.log(`App listening on port ${EXPRESS_PORT}`);
 });
